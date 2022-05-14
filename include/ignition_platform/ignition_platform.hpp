a774999beaec088faaca8ef34dadea9688d63aac
RPS98 <rafa.perez.s98@gmail.com>
2022-05-14 14:54:50 +0200

    Add copyright

--- include/ignition_platform/ignition_platform.hpp
+++ include/ignition_platform/ignition_platform.hpp
@@ -1,3 +1,39 @@
+/*!*******************************************************************************************
+ *  \file       ignition_platform.hpp
+ *  \brief      Implementation of an Ignition Gazebo UAV platform
+ *  \authors    Miguel Fernández Cortizas
+ *              Pedro Arias Pérez
+ *              David Pérez Saura
+ *              Rafael Pérez Seguí
+ *
+ *  \copyright  Copyright (c) 2022 Universidad Politécnica de Madrid
+ *              All Rights Reserved
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ********************************************************************************/
+
 #ifndef IGNITION_PLATFORM_HPP_
 #define IGNITION_PLATFORM_HPP_
 
@@ -7,9 +43,7 @@
 
 #include <Eigen/Dense>
 #include <Eigen/src/Core/Matrix.h>
-
 #include <math.h>
-
 #include <as2_core/core_functions.hpp>
 #include <as2_core/aerial_platform.hpp>
 #include <as2_core/sensor.hpp>
@@ -18,7 +52,6 @@
 #include <geometry_msgs/msg/twist_stamped.hpp>
 #include <nav_msgs/msg/odometry.hpp>
 #include <image_transport/image_transport.hpp>
-
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 #include "ignition_bridge.hpp"
