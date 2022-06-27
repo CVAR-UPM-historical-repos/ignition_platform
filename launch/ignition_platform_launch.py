e54072f72add6800a40b707e2827a33faa50efc8
Pedro Arias <pedroariasperez96@gmail.com>
2022-06-27 11:56:52 +0200

    Reading sensors from ign topic inside launch.py

--- launch/ignition_platform_launch.py
+++ launch/ignition_platform_launch.py
@@ -1,8 +1,44 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, EnvironmentVariable
 from launch_ros.substitutions import FindPackageShare
+
+import subprocess
+
+
+def get_sensors(drone_namespace):
+    sensors  = set()
+
+    cmd = f"ign topic -l"
+    output = subprocess.run(cmd.split(), capture_output=True, text=True)
+    for line in output.stdout.split('\n'):
+        if f"{drone_namespace}/model" in line:
+            tokens = line.split('/')
+            sensors.add(f"{tokens[2]},{tokens[4]},{tokens[6]},{tokens[8]},{tokens[10]}")
+    return repr(sensors).replace("', '", ":")[2:-2] if len(sensors) else ""
+
+
+def get_platform_node(context, *args, **kwargs):
+    drone_namespace = LaunchConfiguration('drone_id').perform(context)
+
+    node = Node(
+        package="ignition_platform",
+        executable="ignition_platform_node",
+        namespace=LaunchConfiguration('drone_id'),
+        output="screen",
+        emulate_tty=True,
+        parameters=[
+            {"control_modes_file": LaunchConfiguration('control_modes_file'),
+            "simulation_mode": True,
+            "mass": LaunchConfiguration('mass'),
+            "max_thrust": LaunchConfiguration('max_thrust'),
+            "min_thrust":  LaunchConfiguration('min_thrust'),
+            "sensors": get_sensors(drone_namespace)
+            }],
+        remappings=[("sensor_measurements/odometry", "self_localization/odom")]
+    )
+    return [node]
 
 
 def generate_launch_description():
@@ -11,28 +47,11 @@ def generate_launch_description():
         'config', 'control_modes.yaml'
     ])
     return LaunchDescription([
-        DeclareLaunchArgument('drone_id', default_value='drone_sim_rafa_0'),
+        DeclareLaunchArgument('drone_id', default_value=EnvironmentVariable('AEROSTACK2_SIMULATION_DRONE_ID')),
         DeclareLaunchArgument('mass', default_value='1.5'),
         DeclareLaunchArgument('max_thrust', default_value='15.0'),
         DeclareLaunchArgument('min_thrust', default_value='0.15'),
-        DeclareLaunchArgument('sensors', default_value='none'),
-
         DeclareLaunchArgument('control_modes_file', default_value=config),
         
-        Node(
-            package="ignition_platform",
-            executable="ignition_platform_node",
-            namespace=LaunchConfiguration('drone_id'),
-            output="screen",
-            emulate_tty=True,
-            parameters=[
-                {"control_modes_file": LaunchConfiguration('control_modes_file'),
-                "simulation_mode": True,
-                "mass": LaunchConfiguration('mass'),
-                "max_thrust": LaunchConfiguration('max_thrust'),
-                "min_thrust":  LaunchConfiguration('min_thrust'),
-                "sensors": LaunchConfiguration('sensors')
-                }],
-            remappings=[("sensor_measurements/odometry", "self_localization/odom")]
-        ),
+        OpaqueFunction(function=get_platform_node)
     ])
