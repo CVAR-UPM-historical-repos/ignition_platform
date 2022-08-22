d060325b59591166a7438aa2afb6625bb440b04d
RPS98 <rafa.perez.s98@gmail.com>
2022-08-22 17:29:14 +0200

    Add targets pose

--- launch/ignition_platform_launch.py
+++ launch/ignition_platform_launch.py
@@ -12,8 +12,9 @@ def get_platform_node(context, *args, **kwargs):
         namespace=LaunchConfiguration('drone_id'),
         output="screen",
         emulate_tty=True,
-        parameters=[
-            {"control_modes_file": LaunchConfiguration('control_modes_file'),
+        parameters=[{
+            "use_sim_time": LaunchConfiguration('use_sim_time'),
+            "control_modes_file": LaunchConfiguration('control_modes_file'),
             "simulation_mode": True,
             "cmd_vel_topic": LaunchConfiguration('cmd_vel_topic')
             }]
@@ -28,6 +29,7 @@ def generate_launch_description():
     ])
     return LaunchDescription([
         DeclareLaunchArgument('drone_id', default_value=EnvironmentVariable('AEROSTACK2_SIMULATION_DRONE_ID')),
+        DeclareLaunchArgument('use_sim_time', default_value='false'),
         DeclareLaunchArgument('control_modes_file', default_value=config),
         DeclareLaunchArgument('cmd_vel_topic', default_value='/cmd_vel'),
         
